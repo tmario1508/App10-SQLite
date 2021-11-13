fa438529c394664311fb8eb94efeab34208db68c
Mario <tmario1508@gmail.com>
2021-11-12 20:28:56 -0600

    first commit

---
+++ App10-SQLite/EditarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditarViewController.swift
+//  App10-SQLite
+//
+//  Created by MTWDM301-2021 on 12/11/21.
+//
+
+import UIKit
+
+class EditarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ App10-SQLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App10-SQLite
+//
+//  Created by MTWDM301-2021 on 12/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
